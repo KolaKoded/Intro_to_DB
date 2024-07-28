e62fb262ba81630f684ad66e0e1f2edb14a9471c
Kolade Fakunle <fakunlekolade@gmail.com>
2024-07-28 09:14:56 +0100

    add script to print full description of books

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Select full description of the Books table
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
